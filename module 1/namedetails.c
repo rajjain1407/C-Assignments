fae54f226eed0d6f169bb85100f348f7f3d6d002
rajjain1407 <110883762+rajjain1407@users.noreply.github.com>
2022-10-10 16:27:39 +0530

    Add files via upload

---
+++ module 1/namedetails.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main(){
+	printf("Name: Rajat Jain\n");
+	printf("DOB: 14/07/1996\n");
+	printf("My age: 26yr\n");
+	printf("Address : Aakash Echo Point, Althan, Surat");
+
+}
